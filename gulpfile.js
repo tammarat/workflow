a8e1011d378ff941fa05fb06f5c885e2fb6cbcbd
Tammarat Watthanaphaiboonsuk <Team@Tammarats-MacBook-Air.local>
2014-11-24 13:49:26 +0700

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
 var jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
@@ -16,6 +18,10 @@ var jsSources = [
 
 
 var sassSources = ['components/sass/style.scss'];
+
+var htmlSources = ['builds/development/*.html'];
+
+var jsonSources = ['builds/development/js/*.json']
 
 
 gulp.task('coffee', function(){
@@ -31,6 +37,7 @@ gulp.task('js', function(){
 	.pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
+	.pipe(connect.reload())
 });
 
 gulp.task('compass', function(){
@@ -42,13 +49,38 @@ gulp.task('compass', function(){
 	})
 	.on('error', gutil.log))
 	.pipe(gulp.dest('builds/development/css'))
+	.pipe(connect.reload())
 });
+
+
 
 gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
+});
+
+gulp.task('connect', function(){
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+
+});
+
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+
+});
+
+gulp.task('json', function(){
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
+
 });
 
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('default', ['coffee', 'js', 'json', 'compass', 'html', 'connect', 'watch']);
